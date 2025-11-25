48688144a379b454e1c456ec6fe3831004e0834f
Blessing Tembo <temboblessing05@gmail.com>
2025-11-25 16:41:42 +0200

    change order and tracking to orderpage and trackingpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { Routes, Route } from "react-router"
 import { HomePage } from "./pages/HomePage"
 import { CheckoutPage } from "./pages/CheckoutPage"
-import { Orders } from "./pages/Orders"
-import { Tracking } from "./pages/Tracking"
+import { OrdersPage } from "./pages/OrdersPage"
+import { TrackingPage } from "./pages/TrackingPage"
 
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage/>} />
-      <Route path="orders" element={<Orders />} />
-      <Route path="tracking" element={<Tracking />}/>
+      <Route path="orders" element={<OrdersPage />} />
+      <Route path="tracking" element={<TrackingPage />}/>
     </Routes>
   )
 }
